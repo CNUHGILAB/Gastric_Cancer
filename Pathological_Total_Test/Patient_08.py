03380cb8fed5c2bdd380a377d95309e61960d267
CNUHGILAB <cnuhgilab@gmail.com>
2023-03-06 15:21:16 +0900

    Pathological Total Test1

---
+++ Pathological_Total_Test/Patient_08.py
@@ -0,0 +1,22 @@
+from Base_ETL import BaseETL
+
+class Patient08(BaseETL):
+
+    def run(self):
+        
+        sql = '''
+            SELECT
+                환자번호,
+                MAX(`수진(진료)일`) AS `수진(진료)일`
+            FROM patient
+            GROUP BY 환자번호
+        '''
+        
+        df = self.df_from_sql(db_name = "gc_raw_test", sql=sql)
+        
+        self.insert(df, db_name = "patient_test", tb_name = "patient_08") 
+
+    
+if __name__ == "__main__":
+    obj = Patient08()
+    obj.run()
