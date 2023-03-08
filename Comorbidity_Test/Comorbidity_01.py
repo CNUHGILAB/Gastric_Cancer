352894c09ffb40b9f381392cc7da36a2381cf9fa
CNUHGILAB <cnuhgilab@gmail.com>
2023-03-08 13:45:42 +0900

    Comorbidity Copy Test3

--- Comorbidity_Test/Comorbidity_01.py
+++ Comorbidity_Test/Comorbidity_01.py
@@ -19,7 +19,7 @@ class Comorbidity01(BaseETL):
             )
             ORDER BY ID ASC
         '''
-        
+
         df = self.df_from_sql(db_name = "gc_raw_test", sql = sql)
         
         self.insert(df, db_name = "gc_protocol_test", tb_name = "comorbidity_01")  # tb_name = "tb_tmp_comorbidity_01"
